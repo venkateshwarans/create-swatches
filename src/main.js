2f201a4ece4e43ea03d7f740dcc0e773183ab339
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2020-02-29 10:26:00 +0530

    creating map between image and hex colors

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import App from './App.svelte';
-import swatches from './palette';
 const app = new App({
 	target: document.body,
 	props: {
@@ -7,17 +6,5 @@ const app = new App({
 	}
 });
 
-for (var swatch in swatches) {
-	// var palette = paletteBoxes[i];
-	if (swatches.hasOwnProperty(swatch) && swatches[swatch]) {
-		// palette.style.color = swatches[swatch].getHex();
-		console.log(swatches[swatch].getHex());
-
-		// palette.querySelector('.palette__value--real').innerHTML = swatches[swatch].getHex();
-
-		// addToPalette(palette, i);
-	}
-	// ++i;
-}
 
 export default app;
